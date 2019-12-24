0f6e834e2760f9f908c3bb5f39a1f659c65d1cc8
Ignacio Bejarano <ignbejarano@gmail.com>
2019-12-24 12:19:59 -0300

    added front end url to cors

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ require('dotenv').load();
 const db = require('./db');
 
 // require('dotenv').config({ path: '.env' });
-
+console.log(process.env.FRONTEND_URL);
 const server = createServer();
 
 server.start(
   {
     cors: {
       credentials: true,
+      origin: process.env.FRONTEND_URL,
     },
   },
   deets => {
